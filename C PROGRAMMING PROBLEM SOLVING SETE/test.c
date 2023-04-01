ae87b29df91b96937d1e9eaf65bba5682c33082f
ROBOT-RIAD <riadhosain420@gmail.com>
2023-04-01 20:09:51 +0600

    c programt

---
+++ C PROGRAMMING PROBLEM SOLVING SETE/test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int aiteam[10]={1,2,3,4,5,6,7,8,9,10};
+    char product_name[10][20]={"ghkl","jfjf","ufhf","usdnfn","sjfnf","ffkf","jfjd","ifngj","fjndsj","fjfjf"};
+    for(int i =0; i<10;i++)
+    {
+        printf("%d\t\t%s\n",aiteam[i],product_name[i]);
+    }
+    
+
+    printf("enter the nemnu number item\n");
+    
+
+}
